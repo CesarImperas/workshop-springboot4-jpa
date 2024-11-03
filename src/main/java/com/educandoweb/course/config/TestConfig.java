767edf75398082634b9ab28d0c5783a3a509d7a2
Caio Cesar <caiocesarvieiracavalcanti@gmail.com>
2024-11-03 16:34:57 -0300

    JPA repository, dependency injection, database seeding

---
+++ src/main/java/com/educandoweb/course/config/TestConfig.java
@@ -0,0 +1,29 @@
+package com.educandoweb.course.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.educandoweb.course.entities.User;
+import com.educandoweb.course.repositories.UserRepository;
+
+@Configuration
+@Profile("test")
+public class TestConfig implements CommandLineRunner {
+	
+	@Autowired
+	private UserRepository userRepository;
+
+	@Override
+	public void run(String... args) throws Exception {
+		// O campo do Id está nulo, pois é responsabilidade do banco de dados, em retornar um Id para o dado
+		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456"); 
+		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456"); 
+		
+		// Método saveAll para salvar os objetos, como lista no parâmetro, no banco de dados 
+		userRepository.saveAll(Arrays.asList(u1, u2));
+	}
+}
